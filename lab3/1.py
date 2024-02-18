fdc6c75d614bc5aa8d5fd146cc2cd899030cf698
saladin <s_shamoi@kbtu.kz>
2024-02-19 03:37:47 +0600

    string upr

---
+++ lab3/1.py
@@ -0,0 +1,9 @@
+class Stringg:
+    def __init__(self):
+        self.string = ""
+
+    def getString(self):
+        self.string = input()
+
+    def printString(self):
+        print(self.string.upper())
