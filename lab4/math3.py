b52d4c31d884f14a88b13638790a5355e42c531b
Saladin <s_shamoi@kbtu.kz>
2024-03-07 10:47:26 +0600

    fix

--- lab4/math3.py
+++ lab4/math3.py
@@ -4,8 +4,8 @@ def a(n_sides, side_length):
     area = (n_sides * side_length ** 2) / (4 * math.tan(math.pi / n_sides))
     return area
 
-n_sides = int(input("Input number of sides: "))
-side_length = float(input("Input the length of a side: "))
+n_sides = int(input())
+side_length = float(input())
 
 area = a(n_sides, side_length)
-print("The area of the polygon is:", area)
+print(area)
